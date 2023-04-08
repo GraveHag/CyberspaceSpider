8aae36521d327bc3ed49de4bc4acdb00866bd64e
Michal Urbánek <michal.urbanek@bluesoft.cz>
2023-04-08 13:01:28 +0200

    Add ToUriList extension

---
+++ CS_Core/Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CS_Core.Extensions
+{
+    internal static class IEnumerableExtensions
+    {
+        internal static IList<Uri> ToUriList(this IEnumerable<string> values)
+        {
+            IList<Uri> result = new List<Uri>();
+            foreach (string value in values)
+            {
+                result.Add(new Uri(value, UriKind.RelativeOrAbsolute));
+            }
+            return result;
+        }
+    }
+}
